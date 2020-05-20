cab04236df7a2c0e3441adf015fcc10585736094
Washington <washingtonshiao@gmail.com>
2020-05-20 08:02:21 -0500

    Can now see what grid row col is clicked

--- static/script.js
+++ static/script.js
@@ -8,7 +8,6 @@ $(document).ready(function () {
     });
     
     function newGame(){
-        console.log($('#row').val());
         $.ajax({
             type: 'GET',
             url: '/newBoard',
@@ -22,7 +21,7 @@ $(document).ready(function () {
                 data.forEach(function(row, i){
                     row.forEach(function (val, j) {
                         var src = `"/static/minesweeper_img/${val}.png"`;
-                        $container.append(`<img id="${i}'x'${j}" class="unclicked" src=${src} alt="field" width="40" height="40">`);
+                        $container.append(`<img id="${i}x${j}" class="unclicked" src=${src} alt="field" width="40" height="40">`);
                     });
                 });
                 $container.css('visibility','visible');
@@ -33,4 +32,10 @@ $(document).ready(function () {
         });
 
     }
+
+    $container.on('click', '.unclicked', function() {
+        var str = $(this).attr('id');
+        var clickedXY = str.split('x');
+        console.log(`${clickedXY[0]} - ${clickedXY[1]} was clicked. Str: ${str}`);
+    });
 });
