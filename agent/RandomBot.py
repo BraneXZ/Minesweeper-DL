f3548eb1c4ef589e7073cc8480e98a72120092c3
Washington <washingtonshiao@gmail.com>
2020-05-20 10:11:26 -0500

    Random bot

---
+++ agent/RandomBot.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 20 10:03:41 2020
+
+@author: Wash
+"""
+
+import numpy as np
+
+from agent.base import Agent
+from minesweeper_board import MineSweeperBoard
+from move import Move
+
+class RandomBot(Agent):
+    def select_move(self, player_board):
+        moves = []
+        
+        for r in range(player_board.shape[0]):
+            for c in range(player_board.shape[1]):
+                if player_board[r][c] == -1:
+                    move = Move(r, c, player_board)
+                    moves.append(move)
+        
+        return np.random.choice(moves)
+    
---
+++ agent/base.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 20 10:02:21 2020
+
+@author: Wash
+"""
+
+class Agent:
+    def __init__(self):
+        pass
+
+    def select_move(self, player_board):
+        raise NotImplementedError()
