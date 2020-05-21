a935770a239cf79c298deb880edb45a29b31a03a
Washington <washingtonshiao@gmail.com>
2020-05-21 11:25:27 -0500

    Helper function that returns an encoder object

--- encoders/base.py
+++ encoders/base.py
@@ -21,3 +21,29 @@ class Encoder:
     def shape(self):
         raise NotImplementedError()
         
+import importlib
+
+def get_encoer_by_name(name, row, col, mine):
+    """
+    
+
+    Parameters
+    ----------
+    name : str
+        Name of the encoder
+    row : int
+        Number of rows 
+    col : int
+        Number of columns
+    mine : int
+        Number of mines
+
+    Returns
+    -------
+    Encoder object
+        Returns the specified encoder object using the name parameter
+
+    """
+    module = importlib.import_module('encoders.' + name)
+    constructor = getattr(module, 'create')
+    return constructor(row, col, mine)
